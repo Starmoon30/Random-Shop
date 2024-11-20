2fcc739962dc84381e10c27124e5bee1cbe6e1b9
xiaojia <12834440+small-leave@user.noreply.gitee.com>
2024-11-20 15:10:26 +0800

    登录页的图片调用改为相对路径

--- Rshop-admin-vue/src/components/Shop-Logo.vue
+++ Rshop-admin-vue/src/components/Shop-Logo.vue
@@ -5,7 +5,7 @@
 <template>
   <!-- Logo 图片 -->
   <div class="logo">
-    <img src="D:\Rshop\Rshop-admin-vue\src\assets\logo.png" alt="Logo" style="width: 300px; height: 80px"/>
+    <img src="@/assets/logo.png" alt="Logo" style="width: 300px; height: 80px"/>
   </div>
 </template>
 
